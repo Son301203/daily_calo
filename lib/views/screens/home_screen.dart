362d9e83ff8063c13ec0b02ac86a61d204f1b93e
Son301203 <121949136+Son301203@users.noreply.github.com>
2025-04-19 11:34:07 +0700

    BottomNavigator (on-going)

--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -1,15 +1,59 @@
+import 'package:daily_calo/views/screens/exercise_screen.dart';
+import 'package:daily_calo/views/screens/meal_screen.dart';
+import 'package:daily_calo/views/screens/profile_screen.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+  int _selectedIndex = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _tabController = TabController(length: 4, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _tabController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Home"
-        ),
+      appBar: AppBar(title: Text("Home")),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          MealScreen(),
+          ExerciseScreen(),
+          ProfileScreen(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+            _tabController.animateTo(index);
+          });
+        },
+        items: [
+          const BottomNavigationBarItem(icon: Icon(Icons.dining_outlined)),
+          const BottomNavigationBarItem(icon: Icon(Icons.healing_outlined)),
+          const BottomNavigationBarItem(icon: Icon(Icons.person_outline)),
+        ],
       ),
     );
   }
