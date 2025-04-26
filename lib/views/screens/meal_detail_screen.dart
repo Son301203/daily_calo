889f66aacf6d1195f7a4bba80bdac6f0c2603cef
Son301203 <121949136+Son301203@users.noreply.github.com>
2025-04-26 11:06:03 +0700

    Meal, Meal Detail, Meal Management UI

--- lib/views/screens/meal_detail_screen.dart
+++ lib/views/screens/meal_detail_screen.dart
@@ -1,14 +1,150 @@
+import 'package:daily_calo/utils/app_color.dart';
 import 'package:flutter/material.dart';
 
 class MealDetailScreen extends StatelessWidget {
-  const MealDetailScreen({super.key});
+  final Map<String, dynamic> dish;
+
+  const MealDetailScreen({super.key, required this.dish});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          "Meal Detail"
+        title: Text(dish['name'], style: TextStyle(color: AppColors.whiteText)),
+        backgroundColor: AppColors.primary,
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Card(
+          elevation: 4,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Title
+                ListTile(
+                  leading: const Icon(
+                    Icons.restaurant_menu,
+                    color: Colors.blue,
+                  ),
+                  title: const Text(
+                    'Tên món ăn',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: AppColors.lightText,
+                    ),
+                  ),
+                  subtitle: Text(
+                    dish['name'],
+                    style: TextStyle(color: AppColors.lightTextSecondary),
+                  ),
+                ),
+                const Divider(),
+                // Serving size
+                ListTile(
+                  leading: const Icon(Icons.fastfood, color: Colors.green),
+                  title: const Text(
+                    'Serving size',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: AppColors.lightText,
+                    ),
+                  ),
+                  subtitle: Text(
+                    '${dish['serving_size']} g',
+                    style: TextStyle(color: AppColors.lightTextSecondary),
+                  ),
+                ),
+                const Divider(),
+                // Protein
+                ListTile(
+                  leading: const Icon(Icons.fitness_center, color: Colors.red),
+                  title: const Text(
+                    'Protein',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: AppColors.lightText,
+                    ),
+                  ),
+                  subtitle: Text(
+                    '${dish['protein']} g',
+                    style: TextStyle(color: AppColors.lightTextSecondary),
+                  ),
+                ),
+                const Divider(),
+                // Period time
+                ListTile(
+                  leading: const Icon(Icons.access_time, color: Colors.purple),
+                  title: const Text(
+                    'Thời gian bữa ăn',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: AppColors.lightText,
+                    ),
+                  ),
+                  subtitle: Text(
+                    dish['period_time'],
+                    style: TextStyle(color: AppColors.lightTextSecondary),
+                  ),
+                ),
+                const Divider(),
+                // Fat
+                ListTile(
+                  leading: const Icon(Icons.local_pizza, color: Colors.orange),
+                  title: const Text(
+                    'Fat',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: AppColors.lightText,
+                    ),
+                  ),
+                  subtitle: Text(
+                    '${dish['fat']} g',
+                    style: TextStyle(color: AppColors.lightTextSecondary),
+                  ),
+                ),
+                const Divider(),
+                // Carbs
+                ListTile(
+                  leading: const Icon(Icons.bakery_dining, color: Colors.brown),
+                  title: const Text(
+                    'Carbs',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: AppColors.lightText,
+                    ),
+                  ),
+                  subtitle: Text(
+                    '${dish['carbs']} g',
+                    style: TextStyle(color: AppColors.lightTextSecondary),
+                  ),
+                ),
+                const Divider(),
+                // Calories
+                ListTile(
+                  leading: const Icon(
+                    Icons.local_fire_department,
+                    color: Colors.redAccent,
+                  ),
+                  title: const Text(
+                    'Calo',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: AppColors.lightText,
+                    ),
+                  ),
+                  subtitle: Text(
+                    '${dish['calories']} kcal',
+                    style: TextStyle(color: AppColors.lightTextSecondary),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
--- lib/views/screens/meal_screen.dart
+++ lib/views/screens/meal_screen.dart
@@ -1,15 +1,188 @@
+import 'package:daily_calo/utils/app_color.dart';
 import 'package:flutter/material.dart';
+import 'manage_meal_screen.dart';
+import 'meal_detail_screen.dart';
 
-class MealScreen extends StatelessWidget {
+class MealScreen extends StatefulWidget {
   const MealScreen({super.key});
 
   @override
+  State<MealScreen> createState() => _MealScreenState();
+}
+
+class _MealScreenState extends State<MealScreen> {
+  final List<String> _meals = ['Bữa sáng', 'Bữa trưa', 'Bữa tối'];
+  int _selectedMealIndex = 0;
+
+  final List<Map<String, dynamic>> _dishes = [
+    {
+      'name': 'Cháo yến mạch',
+      'serving_size': 200,
+      'protein': 5,
+      'period_time': 'Bữa sáng',
+      'fat': 2.5,
+      'carbs': 27,
+      'calories': 150,
+    },
+    {
+      'name': 'Cháo',
+      'serving_size': 250,
+      'protein': 6,
+      'period_time': 'Bữa sáng',
+      'fat': 3.0,
+      'carbs': 30,
+      'calories': 250,
+    },
+    {
+      'name': 'Cơm chiên trứng',
+      'serving_size': 300,
+      'protein': 10,
+      'period_time': 'Bữa trưa',
+      'fat': 12.0,
+      'carbs': 40,
+      'calories': 300,
+    },
+    {
+      'name': 'Salad rau củ',
+      'serving_size': 150,
+      'protein': 2,
+      'period_time': 'Bữa tối',
+      'fat': 1.0,
+      'carbs': 15,
+      'calories': 100,
+    },
+  ];
+
+  @override
   Widget build(BuildContext context) {
+    final filteredDishes = _dishes.where((dish) => dish['period_time'] == _meals[_selectedMealIndex]).toList();
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          "Bữa ăn"
+        title: const Text(
+          'Bữa ăn',
+          style: TextStyle(color: AppColors.whiteText),
         ),
+        backgroundColor: AppColors.primary,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            color: AppColors.whiteText,
+            onPressed: () async {
+              final newDish = await Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const ManageMealScreen()),
+              );
+              if (newDish != null) {
+                setState(() {
+                  _dishes.add(newDish);
+                });
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('Đã thêm ${newDish['name']}')),
+                );
+              }
+            },
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          // Navigator
+          Container(
+            color: Colors.grey[200],
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: _meals.asMap().entries.map((entry) {
+                int index = entry.key;
+                String meal = entry.value;
+                return GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      _selectedMealIndex = index;
+                    });
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 20),
+                    decoration: BoxDecoration(
+                      border: Border(
+                        bottom: BorderSide(
+                          color: _selectedMealIndex == index ? AppColors.primary : Colors.transparent,
+                          width: 2,
+                        ),
+                      ),
+                    ),
+                    child: Text(
+                      meal,
+                      style: TextStyle(
+                        fontWeight: _selectedMealIndex == index ? FontWeight.bold : FontWeight.normal,
+                        color: _selectedMealIndex == index ? AppColors.primary : Colors.black,
+                      ),
+                    ),
+                  ),
+                );
+              }).toList(),
+            ),
+          ),
+          // List Meal
+          Expanded(
+            child: ListView.builder(
+              itemCount: filteredDishes.length,
+              itemBuilder: (context, index) {
+                final dish = filteredDishes[index];
+                return ListTile(
+                  title: Text(dish['name']),
+                  subtitle: Text("${dish['period_time']} - ${dish['calories']} kcal"),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                        icon: const Icon(Icons.edit, color: Colors.blue),
+                        onPressed: () async {
+                          final updatedDish = await Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => ManageMealScreen(dish: dish),
+                            ),
+                          );
+                          if (updatedDish != null) {
+                            setState(() {
+                              final dishIndex = _dishes.indexWhere((item) => item['name'] == dish['name']);
+                              if (dishIndex != -1) {
+                                _dishes[dishIndex] = updatedDish;
+                              }
+                            });
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(content: Text('Đã cập nhật ${updatedDish['name']}')),
+                            );
+                          }
+                        },
+                      ),
+                      IconButton(
+                        icon: const Icon(Icons.delete, color: Colors.red),
+                        onPressed: () {
+                          setState(() {
+                            _dishes.removeWhere((item) => item['name'] == dish['name']);
+                          });
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(content: Text('Đã xóa ${dish['name']}')),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => MealDetailScreen(dish: dish),
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
