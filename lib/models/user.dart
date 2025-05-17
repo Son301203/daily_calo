c732afb902ea6b7f65d7a1c76061ac19b6c8674e
chuthuyquynh1417 <167895440+chuthuyquynh1417@users.noreply.github.com>
2025-05-17 14:59:35 +0700

    update be profile

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,13 +1,44 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
-class User {
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class UserModel {
+  final String uid;
   final String gmail;
   final String name;
   final double height;
   final double weight;
-  User({
+  final DateTime? weightUpdatedAt;
+
+  UserModel({
+    required this.uid,
     required this.gmail,
     required this.name,
     required this.height,
     required this.weight,
+    this.weightUpdatedAt,
   });
+
+  factory UserModel.fromMap(String uid, Map<String, dynamic> map) {
+    return UserModel(
+      uid: uid,
+      gmail: map['gmail'] ?? '',
+      name: map['name'] ?? '',
+      height: (map['height'] ?? 0).toDouble(),
+      weight: (map['weight'] ?? 0).toDouble(),
+      weightUpdatedAt: map['weightUpdatedAt'] != null
+          ? (map['weightUpdatedAt'] as Timestamp).toDate()
+          : null,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'gmail': gmail,
+      'name': name,
+      'height': height,
+      'weight': weight,
+      'weightUpdatedAt': weightUpdatedAt != null
+          ? Timestamp.fromDate(weightUpdatedAt!)
+          : null,
+    };
+  }
 }
